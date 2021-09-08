68c04104321c11320e29e7fc65b34ee9680e67e6
thorbm1500 <43374833+thorbm1500@users.noreply.github.com>
2021-09-08 14:40:03 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static int testing (int value){
+        //return (5 + value);
+
+    }
+
+    public static void main(String[] args) {
+        int value = testing(5);
+        System.out.println(value);
+    }
+
+}
+
+
